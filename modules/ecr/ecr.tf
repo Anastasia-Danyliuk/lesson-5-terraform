6c6fc4526ccb9fee5094c58eb6f913305baa3ac4
bexots64 <bexots64@gmail.com>
2026-07-05 00:22:59 +0300

    Fixed comments

--- modules/ecr/ecr.tf
+++ modules/ecr/ecr.tf
@@ -6,3 +6,16 @@ resource "aws_ecr_repository" "repo" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository" "repo" {
+  name                 = var.ecr_name
+  image_tag_mutability = "MUTABLE"
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+}
--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -40,3 +40,10 @@ resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id
 }
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = { Name = var.vpc_name }
+}
